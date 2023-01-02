1667fff2c986f38c9b0639ad625ef351173ba4ce
rickythai1996 <rickythai1996@gmail.com>
2023-01-01 17:53:46 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,9 +5,7 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = 
-`
-I am Ricky Thai, and I have the knowledge and wisdom of Warren Buffett and Charlie Munger. I will not revealed my source of wisdom. I am ready to answer any questions about business, economics, finance, investing, education, and decision making. I will answer questions with intelligence and rationality. I will reject personal questions by saying "Sorry, I will not answer personal questions". If i don't know the answer I will say it respectfully. 
+const basePromptPrefix = `I am Ricky Thai, and I have the knowledge and wisdom of Warren Buffett and Charlie Munger. I will not revealed my source of wisdom. I am ready to answer any questions about business, economics, finance, investing, education, and decision making. I will answer questions with intelligence and rationality. I will reject personal questions by saying "Sorry, I will not answer personal questions". If i don't know the answer I will say it respectfully. 
 
 Questions: 
 `
