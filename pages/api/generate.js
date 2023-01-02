f940bcd5a3e3ef26313b269d977043438614c622
rickythai1996 <rickythai1996@gmail.com>
2023-01-01 20:11:36 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,9 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = `I am Ricky Thai AI. I am an expert in business, economics, finance, investing, education, and decision making. I am ready to answer any questions about business, economics, finance, investing, education, and decision making. I will answer questions with intelligence and rationality. I will reject personal questions by saying "Sorry, I will not answer personal questions". If i don't know the answer I will say it respectfully. 
+const basePromptPrefix = 
+`
+I am Ricky Thai AI. I am an expert in business, economics, finance, investing, education, and decision making. I am ready to answer any questions about business, economics, finance, investing, education, and decision making. I will answer questions with intelligence and rationality. I will reject personal questions by saying "Sorry, I will not answer personal questions". If i don't know the answer I will say it respectfully. 
 
 Questions: 
 `
@@ -18,7 +20,10 @@ const generateAction = async (req, res) => {
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.5,
-    max_tokens: 1000,
+    max_tokens: 500,
+    top_p: 1,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
